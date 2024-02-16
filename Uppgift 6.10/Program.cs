c96d26d04c10bd5f748e03cf11f5eede0c9a442c
Admin <Admin@TEB12705>
2024-02-16 10:31:48 +0100

    Add project files.

---
+++ Uppgift 6.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace uppgift6_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening som innehåller tal så kommer programmet räkna antalet tal du skrev");
+            string svar = Console.ReadLine();
+            int antalTal = AntalTalIText(svar);
+            Console.WriteLine();
+            Console.WriteLine($"Det finns {antalTal} tal i texten.");
+            Console.ReadKey();
+        }
+        static int AntalTalIText(string mening)
+        {
+            int antal = 0;
+            string[] ord = mening.Split();
+            for (int i = 0; i < ord.Length; i++)
+            {
+                bool checka = double.TryParse(ord[i], out double tal);
+                if (checka)
+                {
+                    antal++;
+                }
+            }
+            return antal;
+        }
+    }
+}
